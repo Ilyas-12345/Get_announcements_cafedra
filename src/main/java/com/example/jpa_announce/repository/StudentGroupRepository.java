877328b10a50bd89497953842514b1d5833d5ecd
Ilyas-12345 <112771019+Ilyas-12345@users.noreply.github.com>
2024-04-01 10:15:05 +0300

    Delete src directory

--- src/main/java/com/example/jpa_announce/repository/StudentGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpa_announce.repository;
-
-import com.example.jpa_announce.entity.StudentGroup;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentGroupRepository extends CrudRepository<StudentGroup,Long> {
-}
