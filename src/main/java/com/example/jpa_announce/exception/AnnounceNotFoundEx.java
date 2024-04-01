7ed8c567317e136b150f07b4002350216e06734f
Ilyas-12345 <112771019+Ilyas-12345@users.noreply.github.com>
2024-04-01 10:23:51 +0300

    Delete src directory

--- src/main/java/com/example/jpa_announce/exception/AnnounceNotFoundEx.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpa_announce.exception;
-
-public class AnnounceNotFoundEx extends Exception {
-    public AnnounceNotFoundEx(String message) {
-        super(message);
-    }
-}
